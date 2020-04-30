17ae939390c91bd1e0009f3b00dd65ef255dc2d8
Wolf2323 <gabrielpatrikurban@gmail.com>
2020-04-30 20:08:02 +0200

    New Build Pipeline with new project structure
    - removed multiVersionPlugin
    - github actions implementation(build.yml)
      - added automatic changelog for release(new Changelog format)
      - deleted .travis.yml
    - added a new Updater to fit the new versioning
    - new discord.sh script for the Discord webhook
    - new mkdocs.sh script for uploading the docs to github pages
    - versioning for mkdocs and fixed version_select.js script

--- scripts/discord.sh
+++ scripts/discord.sh
@@ -1,56 +1,110 @@
 #!/bin/bash
-
-if [ $TRAVIS_PULL_REQUEST == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] ; then
-
-    echo -e "[Webhook]: Sending webhook to Discord...\\n";
-
-    WEBHOOK_URL="https://discordapp.com/api/webhooks/$DISCORD_WEBHOOK_ID/$DISCORD_WEBHOOK_TOKEN"
-
-    AUTHOR_NAME="$(git log -1 "$TRAVIS_COMMIT" --pretty="%aN")"
-    COMMITTER_NAME="$(git log -1 "$TRAVIS_COMMIT" --pretty="%cN")"
-    COMMIT_SUBJECT="$(git log -1 "$TRAVIS_COMMIT" --pretty="%s")"
-    COMMIT_MESSAGE="$(git log -1 "$TRAVIS_COMMIT" --pretty="%b" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')"
-
-    if [ "$AUTHOR_NAME" == "$COMMITTER_NAME" ]; then
-      CREDITS="$AUTHOR_NAME authored & committed"
-    else
-      CREDITS="$AUTHOR_NAME authored & $COMMITTER_NAME committed"
-    fi
-
-    TIMESTAMP=$(date --utc +%FT%TZ)
-
-    WEBHOOK_DATA='{
-      "username": "",
-      "avatar_url": "",
-      "embeds": [ {
-        "color": 13012051,
-        "author": {
-          "name": "Dev build #'"$TRAVIS_BUILD_NUMBER"' is now available",
-          "url": "https://betonquest.pl/"
-        },
-        "title": "'"$COMMIT_SUBJECT"'",
-        "url": "https://github.com/'"$TRAVIS_REPO_SLUG"'/commit/'"$TRAVIS_COMMIT"'",
-        "description": "'"${COMMIT_MESSAGE//$'\n'/ }"\\n\\n"$CREDITS"'",
-        "fields": [
-          {
-            "name": "Commit",
-            "value": "'"[\`${TRAVIS_COMMIT:0:7}\`](https://github.com/$TRAVIS_REPO_SLUG/commit/$TRAVIS_COMMIT)"'",
-            "inline": true
-          },
-          {
-            "name": "Branch",
-            "value": "'"[\`$TRAVIS_BRANCH\`](https://github.com/$TRAVIS_REPO_SLUG/tree/$TRAVIS_BRANCH)"'",
-            "inline": true
-          }
-        ],
-        "thumbnail": {
-          "url": "https://cdn.discordapp.com/app-icons/494162764403572748/d7680ea0dfa2d1da1a95676435f526c1.png?size=64"
-        },
-        "timestamp": "'"$TIMESTAMP"'"
-        } ]
-    }'
-
-    (curl --fail --progress-bar -A "TravisCI-Webhook" -H Content-Type:application/json -H X-Author:ungefroren#7524 -d "${WEBHOOK_DATA//	/ }" "$WEBHOOK_URL" \
-      && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook:\\n\\nWEBHHOK_DATA:\\n$WEBHOOK_DATA"
-
+if [ -z "$2" ]; then
+  echo -e "WARNING!!\nYou need to pass the WEBHOOK_URL environment variable as the second argument to this script.\nFor details & guide, visit: https://github.com/DiscordHooks/github-actions-discord-webhook" && exit
 fi
+
+echo -e "[Webhook]: Sending webhook to Discord...\\n";
+
+AVATAR="https://github.com/actions.png"
+BETON_QUEST_URL="https://avatars1.githubusercontent.com/u/62897788?s=200&v=4"
+COMMIT_ICON_SUCCESS="https://github.githubassets.com/images/icons/emoji/unicode/1f527.png"
+COMMIT_ICON_FAILURE="https://github.githubassets.com/images/icons/emoji/unicode/1f6a8.png"
+COMMIT_ICON_RELEASE="https://github.githubassets.com/images/icons/emoji/unicode/2714.png"
+
+AUTHOR_NAME="$(git log -1 "$GITHUB_SHA" --pretty="%aN")"
+COMMITTER_NAME="$(git log -1 "$GITHUB_SHA" --pretty="%cN")"
+COMMIT_SUBJECT="$(git log -1 "$GITHUB_SHA" --pretty="%s")"
+COMMIT_MESSAGE="$(git log -1 "$GITHUB_SHA" --pretty="%b" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')"
+COMMIT_URL="https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+
+# If, for example, $GITHUB_REF = refs/heads/feature/example-branch
+# Then this sed command returns: feature/example-branch
+BRANCH_NAME="$(echo $GITHUB_REF | sed 's/^[^/]*\/[^/]*\///g')"
+REPO_URL="https://github.com/$GITHUB_REPOSITORY"
+BRANCH_URL="$REPO_URL/tree/$BRANCH_NAME"
+ACTION_URL="$COMMIT_URL/checks"
+if [ "$AUTHOR_NAME" == "$COMMITTER_NAME" ]; then
+  CREDITS="$AUTHOR_NAME authored & committed"
+else
+  CREDITS="$AUTHOR_NAME authored & $COMMITTER_NAME committed"
+fi
+
+LINEBREAK=""
+if [ -n "$COMMIT_MESSAGE" ]; then
+  LINEBREAK="\n"
+fi
+ADDITIONAL_INFORMATION="$LINEBREAK${COMMIT_MESSAGE//$\\n/ }$LINEBREAK\\n$CREDITS"
+
+if [ "$3" = "release" ]; then
+  RELEASE_NAME="RELEASE"
+  RELEASE_DOWNLOAD_URL="https://github.com/BetonQuest/BetonQuest/releases"
+  RELEADE_COMMIT_ICON_SUCCESS=$COMMIT_ICON_RELEASE
+else
+  RELEASE_NAME="Dev-Build"
+  RELEASE_DOWNLOAD_URL="https://betonquest.pl/"
+  RELEADE_COMMIT_ICON_SUCCESS=$COMMIT_ICON_SUCCESS
+fi
+case $1 in
+  "Success" )
+    EMBED_COLOR=3066993
+    DEV_BUILD_DOWNLOAD="Click to Download "$VERSION"!"
+    STATUS_MESSAGE="$RELEASE_NAME is now available"
+    BUILD_DOWNLOAD_URL=$RELEASE_DOWNLOAD_URL
+    DESCRIPTION="${RELEASE_NAME}s can contain bugs, please report them [HERE](https://github.com/BetonQuest/BetonQuest/issues)"
+    COMMIT_ICON=$RELEADE_COMMIT_ICON_SUCCESS
+    ;;
+
+  "Failure" )
+    EMBED_COLOR=15158332
+    STATUS_MESSAGE="There was an error building a $RELEASE_NAME!"
+    DEV_BUILD_DOWNLOAD="Inspect the failure on "$VERSION"!"
+    BUILD_DOWNLOAD_URL=$COMMIT_URL
+    DESCRIPTION=""
+    COMMIT_ICON=$COMMIT_ICON_FAILURE
+    ;;
+
+  * )
+    EMBED_COLOR=0
+    STATUS_MESSAGE="Status Unknown"
+    DEV_BUILD_DOWNLOAD="Inspect the build!"
+    BUILD_DOWNLOAD_URL=$ACTION_URL
+    DESCRIPTION=""
+    COMMIT_ICON=$COMMIT_ICON_FAILURE
+    ;;
+esac
+
+TIMESTAMP=$(date -u +%FT%TZ)
+WEBHOOK_DATA='{
+  "username": "GitHub Actions",
+  "avatar_url": "'$AVATAR'",
+  "embeds": [ {
+    "color": '$EMBED_COLOR',
+    "author": {
+      "name": "'"$STATUS_MESSAGE"'",
+      "url": "'$ACTION_URL'",
+      "icon_url": "'$COMMIT_ICON'" 
+    },
+    "title": "'"${DEV_BUILD_DOWNLOAD}"'",
+    "url": "'"$BUILD_DOWNLOAD_URL"'",
+    "description": "'"$DESCRIPTION"'\n\n'"[__**${COMMIT_SUBJECT}**__](${COMMIT_URL})\\n${ADDITIONAL_INFORMATION}"'",
+    "fields": [
+      {
+        "name": "Commit",
+        "value": "'"[\`${GITHUB_SHA:0:7}\`](${COMMIT_URL})"'",
+        "inline": true
+      },
+      {
+        "name": "Branch",
+        "value": "'"[\`${BRANCH_NAME}\`](${BRANCH_URL})"'",
+        "inline": true
+      }
+    ],
+    "thumbnail": {
+      "url": "'"$BETON_QUEST_URL"'"
+    },
+    "timestamp": "'"$TIMESTAMP"'"
+  } ]
+}'
+
+(curl --fail --progress-bar -A "GitHub-Actions-Webhook" -H Content-Type:application/json -H X-Author:k3rn31p4nic#8383 -d "${WEBHOOK_DATA//	/ }" "$2" \
+  && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook."
