3b6d278004baadecb6342a75c4a252368f70586a
SaltyAimbOtter <saltyaimbotter@gmail.com>
2021-02-04 14:03:27 +0100

    add TotemNotifyIO

---
+++ src/main/java/org/betonquest/betonquest/notify/TotemNotifyIO.java
@@ -0,0 +1,61 @@
+package org.betonquest.betonquest.notify;
+
+import lombok.CustomLog;
+import org.betonquest.betonquest.BetonQuest;
+import org.betonquest.betonquest.compatibility.protocollib.wrappers.WrapperPlayServerEntityStatus;
+import org.betonquest.betonquest.compatibility.protocollib.wrappers.WrapperPlayServerSetSlot;
+import org.betonquest.betonquest.exceptions.InstructionParseException;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Map;
+
+@CustomLog
+public class TotemNotifyIO extends NotifyIO {
+
+    private final int customModelData;
+
+    public TotemNotifyIO(final Map<String, String> data) throws InstructionParseException {
+        super(data);
+        customModelData = getIntegerData("custommodeldata", 2);
+    }
+
+    @Override
+    protected void notifyPlayer(final String message, final Player player) {
+        sendOffhandPacket(player, buildFakeTotem());
+        playSilentTotemEffect(player);
+        sendOffhandPacket(player, player.getInventory().getItemInOffHand());
+    }
+
+
+    private ItemStack buildFakeTotem() {
+        final ItemStack fakeTotem = new ItemStack(Material.TOTEM_OF_UNDYING);
+        final ItemMeta totemMeta = fakeTotem.getItemMeta();
+        totemMeta.setCustomModelData(customModelData);
+        fakeTotem.setItemMeta(totemMeta);
+        return fakeTotem;
+    }
+
+    private void sendOffhandPacket(final Player player, final ItemStack offHandItem) {
+        final WrapperPlayServerSetSlot slotPacket = new WrapperPlayServerSetSlot();
+        slotPacket.setSlot(45);
+        slotPacket.setSlotData(offHandItem);
+        slotPacket.setWindowId(0);
+        slotPacket.sendPacket(player);
+
+    }
+
+    private void playSilentTotemEffect(final Player player) {
+        final WrapperPlayServerEntityStatus statusPacket = new WrapperPlayServerEntityStatus();
+        statusPacket.setEntityStatus((byte) 35);
+        statusPacket.setEntityID(player.getEntityId());
+        statusPacket.sendPacket(player);
+
+        Bukkit.getScheduler().runTask(BetonQuest.getInstance(), () -> player.stopSound(Sound.ITEM_TOTEM_USE, SoundCategory.PLAYERS));
+    }
+}
