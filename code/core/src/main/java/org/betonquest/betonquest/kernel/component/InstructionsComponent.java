83042b83daadecf1071799e946adfbe9520b4fe6
Juyas <juyasetgames@gmail.com>
2026-02-21 12:46:48 +0100

    Integrate ArgumentParsers into InstructionsComponent as the only cyclomatic dependency that cannot be entirely resolved currently

--- code/core/src/main/java/org/betonquest/betonquest/kernel/component/InstructionsComponent.java
+++ code/core/src/main/java/org/betonquest/betonquest/kernel/component/InstructionsComponent.java
@@ -1,12 +1,20 @@
 package org.betonquest.betonquest.kernel.component;
 
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 import org.betonquest.betonquest.api.config.quest.QuestPackageManager;
 import org.betonquest.betonquest.api.instruction.argument.ArgumentParsers;
+import org.betonquest.betonquest.api.instruction.argument.parser.DefaultArgumentParsers;
 import org.betonquest.betonquest.api.logger.BetonQuestLoggerFactory;
 import org.betonquest.betonquest.api.service.DefaultInstructions;
+import org.betonquest.betonquest.api.service.identifier.Identifiers;
 import org.betonquest.betonquest.api.service.instruction.Instructions;
+import org.betonquest.betonquest.api.service.item.ItemManager;
+import org.betonquest.betonquest.api.text.TextParser;
+import org.betonquest.betonquest.id.item.ItemIdentifierFactory;
 import org.betonquest.betonquest.kernel.AbstractCoreComponent;
 import org.betonquest.betonquest.kernel.processor.quest.PlaceholderProcessor;
+import org.bukkit.Server;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Set;
@@ -30,14 +38,18 @@ public class InstructionsComponent extends AbstractCoreComponent {
     }
 
     @Override
+    public <U> void inject(final Class<U> dependencyClass, final U component) {
+        this.injectedDependencies.add(new InjectedDependency<>(dependencyClass, component));
+    }
+
+    @Override
     public Set<Class<?>> provides() {
         return Set.of(Instructions.class);
     }
 
     @Override
     public Set<Class<?>> requires() {
-        return Set.of(QuestPackageManager.class, BetonQuestLoggerFactory.class, ArgumentParsers.class,
-                PlaceholderProcessor.class);
+        return Set.of(QuestPackageManager.class, BetonQuestLoggerFactory.class);
     }
 
     @Override
@@ -49,11 +61,16 @@ public class InstructionsComponent extends AbstractCoreComponent {
     public void load(final DependencyProvider providerCallback) {
         final QuestPackageManager questPackageManager = getDependency(QuestPackageManager.class);
         final BetonQuestLoggerFactory loggerFactory = getDependency(BetonQuestLoggerFactory.class);
-        final ArgumentParsers argumentParsers = getDependency(ArgumentParsers.class);
-        final PlaceholderProcessor placeholderProcessor = getDependency(PlaceholderProcessor.class);
 
-        this.instructions = new DefaultInstructions(() -> placeholderProcessor, () -> questPackageManager, () -> argumentParsers, () -> loggerFactory);
+        this.instructions = new DefaultInstructions(Suppliers.memoize(() -> getDependency(PlaceholderProcessor.class)),
+                () -> questPackageManager,
+                Suppliers.memoize(getArgumentParsers()),
+                () -> loggerFactory);
 
         providerCallback.take(Instructions.class, instructions);
+    }
+
+    private Supplier<ArgumentParsers> getArgumentParsers() {
+        return () -> new DefaultArgumentParsers(getDependency(ItemManager.class), getDependency(ItemIdentifierFactory.class), getDependency(TextParser.class), getDependency(Server.class), getDependency(Identifiers.class));
     }
 }
