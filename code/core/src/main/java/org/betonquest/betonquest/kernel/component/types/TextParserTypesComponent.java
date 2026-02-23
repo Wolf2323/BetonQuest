b3ed9372c745eb9f1f9934a6711997c7de1a6d5f
Juyas <juyasetgames@gmail.com>
2026-02-23 15:45:05 +0100

    Add TextParserTypesComponent to load text parser types

---
+++ code/core/src/main/java/org/betonquest/betonquest/kernel/component/types/TextParserTypesComponent.java
@@ -0,0 +1,55 @@
+package org.betonquest.betonquest.kernel.component.types;
+
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.betonquest.betonquest.api.text.TextParserRegistry;
+import org.betonquest.betonquest.kernel.AbstractCoreComponent;
+import org.betonquest.betonquest.kernel.DependencyProvider;
+import org.betonquest.betonquest.text.parser.LegacyParser;
+import org.betonquest.betonquest.text.parser.MineDownParser;
+import org.betonquest.betonquest.text.parser.MiniMessageParser;
+import org.bukkit.ChatColor;
+
+import java.util.Set;
+
+/**
+ * The {@link AbstractCoreComponent} loading text parser types.
+ */
+public class TextParserTypesComponent extends AbstractCoreComponent {
+
+    /**
+     * Create a new TextParserTypesComponent.
+     */
+    public TextParserTypesComponent() {
+        super();
+    }
+
+    @Override
+    public Set<Class<?>> requires() {
+        return Set.of(TextParserRegistry.class);
+    }
+
+    @Override
+    protected void load(final DependencyProvider dependencyProvider) {
+        final TextParserRegistry textParserRegistry = getDependency(TextParserRegistry.class);
+
+        final LegacyComponentSerializer legacySerializer = LegacyComponentSerializer.builder()
+                .hexColors()
+                .useUnusualXRepeatedCharacterHexFormat()
+                .extractUrls()
+                .build();
+        textParserRegistry.register("legacy", new LegacyParser(legacySerializer));
+        final MiniMessage miniMessage = MiniMessage.miniMessage();
+        textParserRegistry.register("minimessage", new MiniMessageParser(miniMessage));
+        final MiniMessage legacyMiniMessage = MiniMessage.builder()
+                .preProcessor(input -> {
+                    final TextComponent deserialize = legacySerializer.deserialize(ChatColor.translateAlternateColorCodes('&', input.replaceAll("(?<!\\\\)\\\\n", "\n")));
+                    final String serialize = miniMessage.serialize(deserialize);
+                    return serialize.replaceAll("\\\\<", "<");
+                })
+                .build();
+        textParserRegistry.register("legacyminimessage", new MiniMessageParser(legacyMiniMessage));
+        textParserRegistry.register("minedown", new MineDownParser());
+    }
+}
