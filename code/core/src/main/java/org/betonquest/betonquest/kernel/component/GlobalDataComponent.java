ce9b5c18e2139e78411420117b144bd110538d31
Juyas <juyasetgames@gmail.com>
2026-02-24 11:05:47 +0100

    Add GlobalDataComponent to load GlobalData class

---
+++ code/core/src/main/java/org/betonquest/betonquest/kernel/component/GlobalDataComponent.java
@@ -0,0 +1,39 @@
+package org.betonquest.betonquest.kernel.component;
+
+import org.betonquest.betonquest.api.logger.BetonQuestLoggerFactory;
+import org.betonquest.betonquest.database.Connector;
+import org.betonquest.betonquest.database.GlobalData;
+import org.betonquest.betonquest.database.Saver;
+import org.betonquest.betonquest.kernel.AbstractCoreComponent;
+import org.betonquest.betonquest.kernel.DependencyProvider;
+
+import java.util.Set;
+
+/**
+ * The implementation of {@link AbstractCoreComponent} for {@link GlobalData}.
+ */
+public class GlobalDataComponent extends AbstractCoreComponent {
+
+    /**
+     * Create a new GlobalDataComponent.
+     */
+    public GlobalDataComponent() {
+        super();
+    }
+
+    @Override
+    public Set<Class<?>> requires() {
+        return Set.of(BetonQuestLoggerFactory.class, Saver.class, Connector.class);
+    }
+
+    @Override
+    protected void load(final DependencyProvider dependencyProvider) {
+        final BetonQuestLoggerFactory loggerFactory = getDependency(BetonQuestLoggerFactory.class);
+        final Saver saver = getDependency(Saver.class);
+        final Connector connector = getDependency(Connector.class);
+
+        final GlobalData globalData = new GlobalData(loggerFactory.create(GlobalData.class), saver, connector);
+
+        dependencyProvider.take(GlobalData.class, globalData);
+    }
+}
