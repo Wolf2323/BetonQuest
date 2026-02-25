9069ea31bc1ddbf72a27eced662c8a8dac9e2b4f
Juyas <juyasetgames@gmail.com>
2026-02-25 12:38:41 +0100

    Add VersionInfoComponent to display BetonQuest version and JRE info

---
+++ code/core/src/main/java/org/betonquest/betonquest/kernel/component/VersionInfoComponent.java
@@ -0,0 +1,42 @@
+package org.betonquest.betonquest.kernel.component;
+
+import org.betonquest.betonquest.api.logger.BetonQuestLogger;
+import org.betonquest.betonquest.api.logger.BetonQuestLoggerFactory;
+import org.betonquest.betonquest.kernel.AbstractCoreComponent;
+import org.betonquest.betonquest.kernel.DependencyProvider;
+import org.betonquest.betonquest.versioning.java.JREVersionPrinter;
+import org.bukkit.plugin.PluginDescriptionFile;
+
+import java.util.Set;
+
+/**
+ * The implementation of {@link AbstractCoreComponent} for version information.
+ */
+public class VersionInfoComponent extends AbstractCoreComponent {
+
+    /**
+     * Create a new VersionInfoComponent.
+     */
+    public VersionInfoComponent() {
+        super();
+    }
+
+    @Override
+    public Set<Class<?>> requires() {
+        return Set.of(PluginDescriptionFile.class, BetonQuestLoggerFactory.class);
+    }
+
+    @Override
+    protected void load(final DependencyProvider dependencyProvider) {
+        final PluginDescriptionFile descriptionFile = getDependency(PluginDescriptionFile.class);
+        final BetonQuestLoggerFactory loggerFactory = getDependency(BetonQuestLoggerFactory.class);
+
+        final BetonQuestLogger log = loggerFactory.create(VersionInfoComponent.class);
+
+        final JREVersionPrinter jreVersionPrinter = new JREVersionPrinter();
+        final String jreInfo = jreVersionPrinter.getMessage();
+        final String version = descriptionFile.getVersion();
+        log.debug("BetonQuest " + version + " is starting...");
+        log.info(jreInfo);
+    }
+}
